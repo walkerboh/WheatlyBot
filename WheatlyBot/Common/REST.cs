723459f9a9890d9c559269fefe26faf4cbf931c2
Evan Frisch <walkerboh1213@gmail.com>
2018-01-11 19:20:33 -0500

    Clean up and added a missing exception handling

--- WheatlyBot/Common/REST.cs
+++ WheatlyBot/Common/REST.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,20 +7,32 @@ namespace WheatlyBot.Common
 {
     public static class REST
     {
+        private static Logger Logger = LogManager.GetCurrentClassLogger();
+
         public static async Task<T> Get<T>(string baseURI, string requestURI = "")
         {
             T ret = default(T);
 
-            HttpClient client = new HttpClient();
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri(baseURI)
+            };
 
-            client.BaseAddress = new Uri(baseURI);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-            HttpResponseMessage response = await client.GetAsync(requestURI);
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(requestURI);
 
-            if (response.IsSuccessStatusCode)
-                ret = await response.Content.ReadAsAsync<T>();
+                if (response.IsSuccessStatusCode)
+                    ret = await response.Content.ReadAsAsync<T>();
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Error in REST.Get<T>");
+            }
 
             return ret;
         }
